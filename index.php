c49cd22aaacd9b33708bff3bfa481d986cdac718
Rozmaring <ritaratkai@yahoo.com>
2017-09-28 16:38:20 +0200

    ajouter bootstrap design

--- index.php
+++ index.php
@@ -5,74 +5,100 @@
 		<link rel="stylesheet" href="website/images">
 	  <link type="text/css" rel="stylesheet" href="css/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-  <title>Legrand</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		  <title>Legrand</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 
 <body>
 
+	<fieldset id="base">
 
+	<div class="container">
 
-<fieldset>
+			<div class="col-md-6">
 
-    <legend><img class="legrandlogo" src="photos/legrandlogo.png" alt="legrandlogo"/> Peser des rebuts </legend>
+				<div>
+						<br><img class="legrandlogo" src="photos/legrandlogo.png" alt="legrandlogo"/></br>
+						<br><h1>Application "peser des rebuts"</h1></br>
+				</div>
 
-		<section id="formulaire">
+				<form action="peser.php" method='POST'>
+				<div class="form-group">
+			     <label for="exampleInputName2">Code_barre_production :</label>
+			     <input type="text" class="form-control" id="exampleInputName2" placeholder="Code_barre_production" name="code_barre_production">
+			   </div>
+				 <div class="form-group">
+		 	     <label for="exampleInputName2">Code_barre_caisse :</label>
+		 	     <input type="text" class="form-control" id="exampleInputName2" placeholder="Code_barre_caisse " name="code_barre_caisse">
+		 	   </div>
+				 <div class="form-group">
+						<label for="exampleInputName2"> Code_barre_peser : </label>
+						<input type="text" class="form-control" id="exampleInputName2" placeholder=" Code_barre_peser "name="code_barre_peser">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputName2">Peser :</label>
+						<input type="text" class="form-control" id="exampleInputName2" placeholder="kg" name="peser">
+					</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+				</form>
 
-    <form action="peser.php" method='POST'>
-      <p>
-      <label for="code_barre_production"> Code_barre_production : </label><input type="text" name="code_barre_production" id="code_barre_production"/><br />
-      <label for="code_barre_caisse"> Code_barre_caisse : </label><input type="text" name="code_barre_caisse" id="code_barre_caisse"/><br />
-			<label for="code_barre_peser"> Code_barre_peser : </label><input type="text" name="code_barre_peser" id="code_barre_peser"/><br />
-      <label for="peser"> Peser / Kg : </label><input type="text" name="peser" id="peser"/><br />
-        <br><input type="submit" value="Peser"/></br>
-      </p>
-    </form>
+			</div>
 
-		</section>
+			<div class="col-md-6">
 
-		<section id="result">
+			<?php
+		// Affiche les Billets
+			try
+			{
+				$bdd = new PDO('mysql:host=localhost;dbname=legrand;charset=utf8', 'root', 'wucly1978');
+			}
+			catch(Exception $e)
+			{
+							die('Erreur : '.$e->getMessage());
+			}
 
-    <?php
-  // Affiche les Billets
-    try
-    {
-      $bdd = new PDO('mysql:host=localhost;dbname=legrand;charset=utf8', 'root', 'wucly1978');
-    }
-    catch(Exception $e)
-    {
-            die('Erreur : '.$e->getMessage());
-    }
+			$reponse = $bdd->query('SELECT * FROM info_donnees info
+															INNER JOIN apres_peser ap
+															ON info.code_barre_peser = ap.code_barre_peser
+															ORDER BY ap.id DESC ');
 
-    $reponse = $bdd->query('SELECT * FROM info_donnees info
-														INNER JOIN apres_peser ap
-														ON info.code_barre_peser = ap.code_barre_peser
-														ORDER BY ap.id DESC ');
+			while ($donnees = $reponse->fetch()){
+			?>
 
-    while ($donnees = $reponse->fetch()){
-    ?>
+					<h3>
+							<em>le <?php echo $donnees['date_peser']; ?></em>
+					</h3>
 
-        <h3>
-            <em>le <?php echo $donnees['date_peser']; ?></em>
-        </h3>
+					<p>
+					<em>Kg : <?php echo nl2br(htmlspecialchars($donnees['peser'])); ?></em>
+					<br />
+					 <em> Code barre de production : <?php echo htmlspecialchars($donnees['code_barre_production']); ?></em>
+					 <br />
+					 <em> Code barre de caisse : <?php echo htmlspecialchars($donnees['code_barre_caisse']); ?></em>
+					<br />
+					<em> Code barre de peser : <?php echo htmlspecialchars($donnees['code_barre_peser']); ?></em>
+					</p>
 
-        <p>
-        <em>Kg : <?php echo nl2br(htmlspecialchars($donnees['peser'])); ?></em>
-        <br />
-         <em> Code barre de production : <?php echo htmlspecialchars($donnees['code_barre_production']); ?></em>
-         <br />
-         <em> Code barre de caisse : <?php echo htmlspecialchars($donnees['code_barre_caisse']); ?></em>
-        <br />
-				<em> Code barre de peser : <?php echo htmlspecialchars($donnees['code_barre_peser']); ?></em>
-				</p>
+			<?php
+			}
+			$reponse->closeCursor();
+			?>
 
-    <?php
-    }
-    $reponse->closeCursor();
-    ?>
+		</div>
 
-	</section>
+	</div>
 
   </fieldset>
 
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
