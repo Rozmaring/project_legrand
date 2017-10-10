0f3daf4cac96945130d6e4f7b4dc57247864c32e
Stephane PACHOT <stef.pachot@gmail.com>
2017-10-10 16:55:08 +0200

    ajout de la prise en charge du scanner

--- index.php
+++ index.php
@@ -2,142 +2,118 @@
 <html lang="fr">
 <head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="website/images">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link type="text/css" rel="stylesheet" href="css/style.css" />
+		<link type="text/css" rel="stylesheet" href="css/style.css">
 		<title>Legrand</title>
 </head>
 
-<body onkeydown="affKeyCode(event)">
-<div class="container" id="main">
-
-	<div class="header clearfix">
+<body>
+	<div class="container" id="main">
+		<div class="header clearfix">
 	        <nav>
 	          <ul class="nav nav-pills pull-left">
 	            <li role="presentation" ><img class="logo" src="photos/logo.jpg" alt="logo"/></li>
-							<li role="presentation" ><h1>Application impression étiquette rebut</h1></li>
-							<li role="presentation" ><img class="legrandlogo" src="photos/legrandlogo.png" alt="legrandlogo"/></li>
+				<li role="presentation" ><h1>Application impression étiquette rebut</h1></li>
+				<li role="presentation" ><img class="legrandlogo" src="photos/legrandlogo.png" alt="legrandlogo"/></li>
 	          </ul>
-					 </nav>
-	</div>
-
-
-	<div class="container-fluid">
-		<div class="row col-md-12">
-	<div class="col-xs-12 col-sm-12">
-		<h2 class="page-header">Déclaration des rebuts</h2>
-	</div>
-	</div>
-</div>
-
-
-	<div class="container-fluid">
-	<div class="row col-xs-12 col-md-12">
+			</nav>
+		</div>
+		<div class="container-fluid">
+			<div class="row col-md-12">
+				<div class="col-xs-12 col-sm-12">
+					<h2 class="page-header">Déclaration des rebuts</h2>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row col-xs-12 col-md-12">
 				<div class="col-xs-6 col-sm-6">
+					<form action="peser.php" method='POST'  onsubmit="return submitControl(false)">
+						<div class="form-group">
+							<label for="InputName1">Code barre production :</label>
+							<input type="text" class="form-control" id="InputName1" placeholder="Code_barre_production" name="code_barre_production" disabled>
+						</div>
 
-				<form action="peser.php" method='POST' onsubmit='return verif_champ(document.form1.mots_cles.value);'>
-				<div class="form-group">
-			     <label for="exampleInputName2">Code_barre_production:</label>
-			     <input type="text" size="10" tabindex="1" class="form-control" id="code_barre_production" placeholder="Code_barre_production" name="code_barre_production" maxlength="10" onkeydown="focusNext(elmnt,content)">
-			   </div>
-				 <div class="form-group">
-		 	     <label for="exampleInputName2">Code_barre_caisse :</label>
-		 	     <input type="text" size="10" tabindex="2" class="form-control" id="code_barre_caisse" placeholder="Code_barre_caisse " name="code_barre_caisse" maxlength="10" onkeyup="toUnicode(this,this.value)">
-		 	   </div>
-				 <div class="form-group">
-						<label for="exampleInputName2"> Code_barre_peser : </label>
-						<input type="text" size="10" tabindex="3" class="form-control" id="code_barre_peser" placeholder=" Code_barre_peser "name="code_barre_peser" maxlength="10" onkeyup="toUnicode(this,this.value)">
-					</div>
-					<div class="form-group">
-						<label for="exampleInputName2">Peser :</label>
-						<input type="text" class="form-control" id="peser" placeholder="kg" name="peser">
-					</div>
+						<div class="form-group">
+							<label for="InputName2">Code barre peser :</label>
+							<input type="text" class="form-control" id="InputName2" placeholder="Code_barre_peser" name="code_barre_peser" disabled>
+						</div>
 
+						<div class="form-group">
+							<label for="InputName3">Code barre caisse :</label>
+							<input type="text" class="form-control" id="InputName3" placeholder="Code_barre_caisse" name="code_barre_caisse" disabled>
+						</div>
+
+						<div class="form-group">
+							<input type="hidden" class="form-control" id="InputName4" placeholder="kg" name="peser_totale">
+						</div>
+				</div>
+						<div class="col-xs-6 col-sm-6">
+							<br><button type="submit" name="valider" class="btn btn-default" disabled>Valider</button></br>
+							<br><button type="reset" name="reset" class="btn btn-default">Réinitialiser</button></br>
+						</div>
+					</form>
 			</div>
-
-			<div class="col-xs-6 col-sm-6">
-				<br><button type="submit" name="valider" class="btn btn-default">Valider</button></br>
-				<br><button type="reset" name="reset" class="btn btn-default">Annuler</button></br>
-			</div>
-
 		</div>
-			</form>
-			</div>
-
-			<div class="container-fluid">
-				<div class="row col-md-12">
-			<div class="col-xs-12 col-sm-12">
-
-				<div class="bs-example">
+		<div class="container-fluid">
+			<div class="row col-md-12">
+				<div class="col-xs-12 col-sm-12">
+					<div class="bs-example">
 						<table class="table table-bordered">
-						<caption>Nomenclature</caption>
-						<thead>
-						<tr>
-						<th>ID</th>
-						<th>Date de peser </th>
-						<th>Heure de peser </th>
-						<th>Code barre de production </th>
-						<th>Code barre de caisse </th>
-						<th>Code barre de peser </th>
-						<th>Kg</th>
-						</tr>
-						</thead>
-						<tbody>
-
-			<?php
-		// Affiche les Billets
-			try
-			{
-				$bdd = new PDO('mysql:host=localhost;dbname=legrand;charset=utf8', 'root', 'wucly1978');
-			}
-			catch(Exception $e)
-			{
-							die('Erreur : '.$e->getMessage());
-			}
-
-			$reponse = $bdd->query('SELECT * FROM info_donnees info
-															INNER JOIN apres_peser ap
-															ON info.code_barre_peser = ap.code_barre_peser
-															ORDER BY ap.id DESC ');
-
-			while ($donnees = $reponse->fetch()){
-			?>
-						<tr>
-
-					<th scope="row"><?php echo $donnees['id']; ?></th>
-					<td><?php echo $donnees['date_peser']= date("Y-m-d"); ?></td>
-					<td><?php echo $donnees['date_peser']= date("H:i:s"); ?></td>
-					<td><?php echo htmlspecialchars($donnees['code_barre_production']); ?></td>
-					<td><?php echo htmlspecialchars($donnees['code_barre_caisse']); ?></td>
-					<td><?php echo htmlspecialchars($donnees['code_barre_peser']); ?></td>
-					<td><?php echo nl2br(htmlspecialchars($donnees['peser'])); ?></td>
-					</tr>
-
-
-			<?php
-			}
-			$reponse->closeCursor();
-			?>
-
-		</tbody>
-					</table>
+							<caption>Nomenclature</caption>
+							<thead>
+								<tr>
+									<th>Code barre de production</th>
+									<th>Poid rebuts</th>
+									<th>Date de peser</th>
+									<th>Heure de peser</th>
+									<th>Code barre de peser</th>
+									<th>Code barre de caisse</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php
+								// Affiche les Billets
+								try
+								{
+									$bdd = new PDO('mysql:host=developont.fr;dbname=developonteur;charset=utf8mb4', 'developonteur', 'G4rdeP0n!');
+								}
+								catch(Exception $e)
+								{
+									die('Erreur : '.$e->getMessage());
+								}
+								$reponse = $bdd->query('SELECT * FROM code_barre info
+														INNER JOIN peser pe
+														ON info.code_barre_production = pe.code_barre_production
+														ORDER BY pe.date_peser DESC ');
+								while ($donnees = $reponse->fetch()){
+							?>
+								<tr>
+									<td scope="row"><?php echo htmlspecialchars($donnees['code_barre_production']); ?></td>
+									<td><?php echo htmlspecialchars($donnees['poid_rebut']); ?></td>
+									<td><?php echo $donnees['date_peser'] ?></td>
+									<td><?php echo $donnees['date_peser'] ?></td>
+									<td><?php echo htmlspecialchars($donnees['code_barre_peser']); ?></td>
+									<td><?php echo htmlspecialchars($donnees['code_barre_caisse']); ?></td>
+								</tr>
+							<?php
+								}
+								$reponse->closeCursor();
+							?>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
-
-</div>
-</div>
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.min.js"></script>
-	<!-- include jQuery via CDN -->
-	 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-	<!-- your own file in your project folder -->
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	<script language="javascript" src="js/scan.js"></script>
 </body>
 </html>
--- peser.php
+++ peser.php
@@ -1,25 +1,26 @@
 <?php
 // Connexion à la base de données
+var_dump($_POST);
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=legrand;charset=utf8', 'root', 'wucly1978');
+	$bdd = new PDO('mysql:host=developont.fr;dbname=developonteur;charset=utf8mb4', 'developonteur', 'G4rdeP0n!');
 }
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
 
-// 1 - Enregistrer dans la base
-$req = $bdd->prepare('INSERT INTO apres_peser (peser, code_barre_peser) VALUES(?, ?)');
-$req->execute(array($_POST['peser'], $_POST['code_barre_peser']));
+// 1 - Enregistrement dans la base
+$req = $bdd->prepare('INSERT INTO peser (code_barre_production, poid_total) VALUES(?, ?)');
+$req->execute(array($_POST['code_barre_production'], $_POST['peser_totale']));
 $req->closeCursor();
 
 // 2 - lier la pesée à la production et à la caisse de pesée
-$req = $bdd->prepare('INSERT INTO info_donnees (code_barre_production, code_barre_caisse, code_barre_peser) VALUES(?, ?, ?)');
+$req = $bdd->prepare('INSERT INTO code_barre (code_barre_production, code_barre_caisse, code_barre_peser) VALUES(?, ?, ?)');
 $req->execute(array($_POST['code_barre_production'], $_POST['code_barre_caisse'], $_POST['code_barre_peser']));
 $req->closeCursor();
 
 
-// Redirection du visiteur vers la page du minichat
+// Redirection du visiteur vers la page principale
 header('Location: index.php');
 ?>
